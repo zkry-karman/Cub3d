cce7d954efb73a151e114f04c6c10cdf16a2bca0
zkarman <zkarman@f1r10s18.paris.42.school>
2026-09-06 16:02:52 +0200

    setting paths to null

--- setup/setup_master_struct.c
+++ setup/setup_master_struct.c
@@ -6,7 +6,7 @@
 /*   By: zkarman <zkarman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 16:15:11 by karmanz           #+#    #+#             */
-/*   Updated: 2026/09/01 15:23:11 by zkarman          ###   ########.fr       */
+/*   Updated: 2026/09/06 16:01:49 by zkarman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,9 @@ void	initialize_master(t_bible *master)
 	master->player.x = -1.0;
 	master->player.y = -1.0;
 	master->player.player_count = 0;
+	master->graphics.no_path = NULL;
+	master->graphics.so_path = NULL;
+	master->graphics.ea_path = NULL;
+	master->graphics.we_path = NULL;
 	initialize_minimap_stats(master);
 }
